792292d5c2350fd62589f214f8e104320b8b4e1a
MilkyNik <milniklaus@gmail.com>
2019-04-16 20:54:00 +0300

    Initial commit

---
+++ MemoryCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemoryCard
+//
+//  Created by iMac on 16.04.19.
+//  Copyright © 2019 hata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
