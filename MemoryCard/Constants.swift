f59d02db5e9a376bcd70dac03a213e5e1e57d64b
MilkyNik <milniklaus@gmail.com>
2019-04-24 22:23:44 +0300

    Added APIClient.

--- MemoryCard/Constants.swift
+++ MemoryCard/Constants.swift
@@ -6,6 +6,9 @@
 //  Copyright © 2019 hata. All rights reserved.
 //
 
+let keyArtworkURL = "artwork_url"
+let keyTracks = "tracks"
+let keyID = "id"
 
 let defaultGridSize = 4 * 4
 let defaultAlertTitle = "Good job!"
